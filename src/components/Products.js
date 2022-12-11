65312a32ce42943a08b660c1230e0fea1fafd1a5
tejasvijb <jbtejasvi@gmail.com>
2022-12-12 00:29:50 +0530

    added images

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,17 @@
 
+import { render } from "@testing-library/react";
 import { Table } from "antd";
 
 export default function Products({ data,selected }) {
     
     const columns = [
+        {
+            title: "Image",
+            dataIndex: "images",
+            render: (images) => {
+                return <img className="thumbnail" src={images[images.length-1]} alt="productimage" height="50px"></img>
+            }
+        },
         {
           title: "Name",
           dataIndex: "title",
@@ -32,6 +40,10 @@ export default function Products({ data,selected }) {
         {
           title: "Brand",
           dataIndex: "brand",
+        },
+        {
+            title: "Category",
+            dataIndex: "category"
         },
         {
           title: "Compare products",
